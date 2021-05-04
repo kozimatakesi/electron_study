b7a3833c062ef9159cd9fdb3235f90da6ac232a0
riverbook708 <riverbook708@gmail.com>
2021-05-04 20:26:01 +0900

    add_bower

--- preload.js
+++ preload.js
@@ -4,15 +4,6 @@ const fileUtil = require('./lib/fileUtil');
 
 contextBridge.exposeInMainWorld(
   "api", {
-    file: (callBack) => {
-      glob('node_modules/**/README.md', function (err, matches) {
-        if(err) {
-          callBack(err, null);
-          return;
-        }
-        callBack(null, matches);
-      });
-    },
     fileTwo: fileUtil
   },
 );
